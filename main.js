0c60d43b43152d2084054e88c6583b5106706eee
msSadm <96068855+msSadm@users.noreply.github.com>
2025-05-19 11:14:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const myImage = document.querySelector("img");
 
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/usa.png");
+  if (mySrc === "firefox-icon.png") {
+    myImage.setAttribute("src", "usa.png");
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute("src", "firefox-icon.png");
   }
 });
 
